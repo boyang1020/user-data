5bf98983af227575d4fa1947cc8c0d5c3b6b3ae5
Bo Yang <bo@kribbz.com>
2019-11-05 20:55:04 -0800

    clean up

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -4,6 +4,7 @@ import { UserService } from './user.service';
 import { forkJoin } from 'rxjs';
 import * as _ from 'lodash';
 import { MatTableDataSource } from '@angular/material/table';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-user',
